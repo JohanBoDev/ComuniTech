066a78d08982e558fe3ac91f1d1c3629b63e3a35
JohanBoDev <bohorquezjohan958@gmail.com>
2025-01-17 18:38:43 -0500

    zz

--- backend/src/routes/pagos.js
+++ backend/src/routes/pagos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { crearCheckoutSession, verPagos, obtenerIngresos } = require('../controllers/pagosController');
+const { crearCheckoutSession, verPagos, obtenerIngresos, eliminarPago } = require('../controllers/pagosController');
 const { verificarToken, verificarRol } = require('../middlewares/authMiddleware');
 
 // Rutas de pagos
@@ -8,6 +8,7 @@ const { verificarToken, verificarRol } = require('../middlewares/authMiddleware'
 router.post('/pagar',verificarToken, crearCheckoutSession); // Iniciar pago
 router.get('/ObtenerPagos', verificarToken, verificarRol([1]), verPagos); // Ver pagos
 router.get('/reporteIngresos', verificarToken, verificarRol([1]), obtenerIngresos); // Reporte de ingresos
+router.delete('/eliminarPago/:id_pago', verificarToken, verificarRol([1]), eliminarPago); // Eliminar pago
 
 
 module.exports = router;
