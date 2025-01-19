79531b763b9150aaba768b39e9db6855c4cd5e24
JohanBoDev <bohorquezjohan958@gmail.com>
2025-01-19 18:28:39 -0500

    ss

--- frontend/src/pages/seccionInicio.jsx
+++ frontend/src/pages/seccionInicio.jsx
@@ -10,7 +10,7 @@ const HomeSection = () => {
       {/* Video de fondo */}
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="/buildComuniTech.mp4"
+        src="/buildComuniTech.mp4(2)"
         autoPlay
         loop
         muted
